8d960c77863daaad6025431a3cabd535b252df49
Yazid <yazidalrasyid821@gmail.com>
2026-05-09 14:47:12 +0700

    Add initial implementation of core application features including app constants, camera service, challenge mode, dashboard, face frame overlay, journal functionality, and model inference service

--- lib/features/challenge/challenge_screen.dart
+++
@@ -1,8 +0,0 @@
-// KONSEP BAGIAN 4: Fitur Tambahan (Anti-Monoton)
-// Challenge Mode: Gamifikasi aplikasi dimana kamera meminta user meniru/meragakan ekspresi tertentu
-// (misal "Coba tampilkan ekspresi Sedih 😢") -> dinilai menggunakan model/layer.
-
-class ChallengeScreen {
-  // TODO: Implementasi state game (Misalnya timer untuk membuat ekspresi bahagia)
-}
-
