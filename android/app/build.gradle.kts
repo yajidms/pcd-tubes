0dadec8a62be9ebbf45ba51dce9ba0d03c3ecfe3
RizkySatria123 <rizky12satria@gmail.com>
2026-06-05 09:28:55 +0700

    debug  build apk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.pcd_tubes"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "28.2.13676358"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
